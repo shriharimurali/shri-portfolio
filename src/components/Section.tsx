f35a73ce7f9967bec59d38615bf566b69062e27a
Shrihari Murali <shriharim6@gmail.com>
2023-06-09 13:55:20 +0530

    Add more SEO related tags

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -16,7 +16,7 @@ export const Section: FC<SectionProps> = ({
       className={`flex flex-col md:flex-row justify-center py-[50px] px-[35px] md:py-[100px] md:px-[170px] w-full max-w-screen-2xl gap-10 md:gap-16 ${className}`}
     >
       {heading && (
-        <h4 className="text-lg text-blue-700 font-semibold">{heading}</h4>
+        <h1 className="text-lg text-blue-700 font-semibold">{heading}</h1>
       )}
       {children}
     </section>
